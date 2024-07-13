c3821c02c936f2d965ea70b2279d10adc5814d04
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2024-07-13 13:27:33 +0100

    anthropic

--- main.py
+++ main.py
@@ -12,15 +12,56 @@ load_dotenv()
 
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
 
-
-load_dotenv()
-
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai = OpenAI(api_key=OPENAI_API_KEY)
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 anthropic = Anthropic(api_key=ANTHROPIC_API_KEY)
+
+URL = "https://www.producthunt.com/leaderboard/daily/2024/7/13?ref=header_nav"
+ITER = 4
+
+SYSTEM_PROMPT = cleandoc(
+    """
+    You are an advanced AI model that can extract structured data from large bodies of text.
+    You think step by step, and apply nuanced reasoning to extract structured data from unstructured text.
+    """
+)
+
+
+def create_extraction_prompt(image_data: str, page_data: str):
+    prompt = (
+        cleandoc(
+            f"""
+                    Given the following extracted structured object from a webpage:
+                    
+                    <structured-subset-data>
+                    {image_data}
+                    </structured-subset-data>
+                    
+                    and the following unstructured markdown data containing the full webpage data:
+                    
+                    <full-page-data>
+                    {page_data}
+                    </full-page-data>
+                    
+                    
+                    Your job is to identify the structured object in the full page data.
+                    You must then identify all the other data in the full page data that shares approximately the same structure as the structured subset data.
+                    You must then extract all of the other sets of data that share the similar structure as the structured subset data.
+                    
+                    Rules for extraction:
+                    - Ignore slight variations in the structure, such as special assets or supporting components.
+                        - Focus mainly on the main data structure, such as the main content or main components.
+                    - You must extract the data in the same JSON format as the structured subset data.
+                    - **You must extract as many sets as possible.**
+                      - If you encounter a set of data that is not in the same JSON format as the structured subset data, you can skip it. Keep extracting the next set of data.
+                    - If you cannot find any data in the full page data that matches the structured subset data, return an empty object.
+                    
+                """
+        ),
+    )
+    return prompt[0]
 
 
 # Function to encode the image
@@ -125,27 +166,44 @@ def extract_full_page_data(image_data: str, page_data: str) -> dict:
     return response.choices[0].message.content
 
 
+def extract_full_page_data_anthropic(image_data: str, page_data: str) -> dict:
+    print("!! extract_full_page_data_anthropic")
+    message = anthropic.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        system=SYSTEM_PROMPT,
+        messages=[
+            {
+                "role": "user",
+                "content": create_extraction_prompt(image_data, page_data),
+            },
+        ],
+    )
+    print(message.content[0].text)
+
+
 if __name__ == "__main__":
     """
-    image_path = "screenshots/3.png"
+    image_path = "screenshots/4.png"
     image_data = extract_data_from_image(image_path)
     print(image_data)
-    url = "https://www.freelancer.com/jobs/artificial-intelligence"
-    page_data = scrape_url(url)
+    # url = "https://www.freelancer.com/jobs/artificial-intelligence"
+    page_data = scrape_url(URL)
     print(page_data)
     # exit the program
     exit()
     """
     # read image_data from `scraped_webpage.md`
-    with open("./scraped_webpages/3.txt", "r") as file:
+    with open(f"./scraped_webpages/{ITER}.txt", "r") as file:
         page_data = file.read()
 
     # read page_data from `image_description.txt`
-    with open("./image_descriptions/3.txt", "r") as file:
+    with open(f"./image_descriptions/{ITER}.txt", "r") as file:
         image_data = file.read()
 
-    print("!! extract_full_page_data")
     print(image_data)
     print()
     print(page_data)
-    full_extracted_data = extract_full_page_data(image_data, page_data)
+    # full_extracted_data = extract_full_page_data(image_data, page_data)
+    extract_full_page_data_anthropic(image_data, page_data)
