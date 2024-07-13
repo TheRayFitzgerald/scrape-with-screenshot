54e2885c2be85f9838f99ad050160bcedc6263ee
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2024-07-13 16:52:20 +0100

    preserve

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import base64
 import time
-from dotenv import load_dotenv
 import os
-from inspect import cleandoc
 
-from openai import OpenAI
+from inspect import cleandoc
+from dotenv import load_dotenv
+
+
 from anthropic import Anthropic
+from openai import OpenAI
 from firecrawl import FirecrawlApp
 
 from colorama import Fore, Style, init
 
-init(autoreset=True)
-
-
-load_dotenv()
 
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai = OpenAI(api_key=OPENAI_API_KEY)
-
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-anthropic = Anthropic(api_key=ANTHROPIC_API_KEY)
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TEST_RUN_DATA_DIR = "test_run_data"
 URL = "https://www.producthunt.com/leaderboard/daily/2024/7/13?ref=header_nav"
 ITER = 4
@@ -30,15 +23,132 @@ ITER = 4
 SYSTEM_PROMPT = cleandoc(
     """
     You are an advanced AI model that can extract structured data from large bodies of text.
+    You excel in scraping key information from webpages and images.
     You think step by step, and apply nuanced reasoning to extract structured data from unstructured text.
     """
 )
 
 
-def create_extraction_prompt(image_data: str, page_data: str):
-    prompt = (
-        cleandoc(
-            f"""
+init(autoreset=True)
+load_dotenv()
+anthropic = Anthropic(api_key=ANTHROPIC_API_KEY)
+openai = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def base64_encode_image(image_path: str) -> str:
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def extract_data_from_image_0(image_path: str) -> str:
+    print("!! extract_data_from_image")
+    # Getting the base64 string
+    base64_image = base64_encode_image(image_path)
+
+    response = openai.chat.completions.create(
+        model="gpt-4o",
+        response_format={"type": "json_object"},
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": cleandoc(
+                            """
+                            Yyou are an expert web scraper from visual images.
+                            Look at this image, extract the key information into a structured JSON format.
+                            Infer what are the appropriate fields and data types.
+                            Ensure that the JSON format is structured, easy to understand, and follows best practices.
+                            """
+                        ),
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+
+    print(response.choices[0].message.content)
+
+    return response.choices[0].message.content
+
+
+def extract_data_from_image(image_path: str) -> str:
+    print("!! extract_data_from_image")
+
+    base64_image = base64_encode_image(image_path)
+
+    message = anthropic.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/png",
+                            "data": base64_image,
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": cleandoc(
+                            """
+                            Yyou are an expert web scraper from visual images.
+                            Look at this image, extract the key information into a structured JSON format.
+                            Infer what are the appropriate fields and data types.
+                            Ensure that the JSON format is structured, easy to understand, and follows best practices.
+                            """
+                        ),
+                    },
+                ],
+            },
+            {
+                "role": "assistant",
+                "content": "{",
+            },
+        ],
+    )
+    # return message.content[0].text but with the `{` added at the beginning
+    return "{" + message.content[0].text
+
+
+def scrape_url(url: str) -> str:
+    print("!! scrape_url")
+    app = FirecrawlApp(api_key=FIRECRAWL_API_KEY)
+
+    try:
+        scraped_data = app.scrape_url(url)
+    except Exception as e:
+        print(f"Unable to scrape the URL: {url}. Error: {e}")
+        return "Unable to scrape the URL."
+    markdown_data = scraped_data.get("markdown")
+
+    return markdown_data
+
+
+def extract_full_page_data(image_data: str, page_data: str) -> dict:
+    print("!! extract_full_page_data")
+
+    with anthropic.messages.stream(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        system=SYSTEM_PROMPT,
+        messages=[
+            {
+                "role": "user",
+                "content": cleandoc(
+                    f"""
                     Given the following extracted structured object from a webpage:
                     
                     <structured-subset-data>
@@ -66,105 +176,7 @@ def create_extraction_prompt(image_data: str, page_data: str):
                     - If you cannot find any data in the full page data that matches the structured subset data, return an empty object.
                     
                 """
-        ),
-    )
-    return prompt[0]
-
-
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
-
-
-def extract_data_from_image(image_path: str) -> str:
-    print("!! extract_data_from_image")
-
-    base64_image = encode_image(image_path)
-
-    message = anthropic.messages.create(
-        model="claude-3-5-sonnet-20240620",
-        max_tokens=1000,
-        temperature=0,
-        system=SYSTEM_PROMPT,
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image",
-                        "source": {
-                            "type": "base64",
-                            "media_type": "image/png",
-                            "data": base64_image,
-                        },
-                    },
-                    {
-                        "type": "text",
-                        "text": cleandoc(
-                            """
-                            Yyou are an expert data scraper from visual images.
-                            Look at this image, extract the key information into a structured JSON format.
-                            Infer what are the appropriate fields and data types.
-                            Ensure that the JSON format is structured, easy to understand, and follows best practices.
-                            **Only include data from the image.** - do not fabricate or infer any additional data.
-                            """
-                        ),
-                    },
-                ],
-            },
-            {
-                "role": "assistant",
-                "content": "{",
-            },
-        ],
-    )
-
-    return message.content[0].text
-
-
-def scrape_url(url: str) -> str:
-    print("!! scrape_url")
-    app = FirecrawlApp(api_key=FIRECRAWL_API_KEY)
-
-    try:
-        scraped_data = app.scrape_url(url)
-    except Exception as e:
-        print(f"Unable to scrape the URL: {url}. Error: {e}")
-        return "Unable to scrape the URL."
-    markdown_data = scraped_data.get("markdown")
-
-    return markdown_data
-
-
-def extract_full_page_data_openai(image_data: str, page_data: str) -> dict:
-    print("!! extract_full_page_data_openai")
-    response = openai.chat.completions.create(
-        model="gpt-4o",
-        response_format={"type": "json_object"},
-        messages=[
-            {
-                "role": "user",
-                "content": create_extraction_prompt(image_data, page_data),
-            },
-        ],
-    )
-
-    return response.choices[0].message.content
-
-
-def extract_full_page_data_anthropic(image_data: str, page_data: str) -> dict:
-    print("!! extract_full_page_data_anthropic")
-
-    with anthropic.messages.stream(
-        model="claude-3-5-sonnet-20240620",
-        max_tokens=1000,
-        temperature=0,
-        system=SYSTEM_PROMPT,
-        messages=[
-            {
-                "role": "user",
-                "content": create_extraction_prompt(image_data, page_data),
+                ),
             },
             {
                 "role": "assistant",
@@ -188,15 +200,12 @@ def test_run():
     with open(f"{TEST_RUN_DATA_DIR}/image_descriptions/{ITER}.txt", "r") as file:
         image_data = file.read()
 
-    extract_full_page_data_anthropic(image_data, page_data)
-
-
-import os
-import time
-from colorama import Fore, Style
+    extract_full_page_data(image_data, page_data)
 
 
 def main():
+    with open(f"{TEST_RUN_DATA_DIR}/scraped_webpages/{ITER}.txt", "r") as file:
+        page_data = file.read()
     while True:
         os.system("rm -rf screenshots/*")
         os.system("mkdir -p screenshots")
@@ -212,39 +221,16 @@ def main():
                 break
 
         image_path = os.path.join("screenshots", os.listdir("screenshots")[0])
+
+        image_data_0 = extract_data_from_image_0(image_path)
+        print(image_data_0)
+
         image_data = extract_data_from_image(image_path)
         print(image_data)
 
-        page_data = scrape_url(URL)
-        extract_full_page_data_anthropic(image_data, page_data)
+        # page_data = scrape_url(URL)
+        # extract_full_page_data(image_data, page_data)
 
 
 if __name__ == "__main__":
     main()
-
-
-if __name__ == "__main__":
-    """
-    image_path = "screenshots/4.png"
-    image_data = extract_data_from_image(image_path)
-    print(image_data)
-    # url = "https://www.freelancer.com/jobs/artificial-intelligence"
-    page_data = scrape_url(URL)
-    print(page_data)
-    # exit the program
-    exit()
-
-    """
-    main()
-    exit()
-    # read image_data from `scraped_webpage.md`
-    with open(f"./scraped_webpages/{ITER}.txt", "r") as file:
-        page_data = file.read()
-
-    # read page_data from `image_description.txt`
-    with open(f"./image_descriptions/{ITER}.txt", "r") as file:
-        image_data = file.read()
-
-    # full_extracted_data = extract_full_page_data(image_data, page_data)
-    extract_full_page_data_anthropic(image_data, page_data)
-    # extract_full_page_data_openai(image_data, page_data)
